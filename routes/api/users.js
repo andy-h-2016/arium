3d65b08f270e8d01034fe5f1a904e0966fac7102
Andy Huang <andy.k.huang@gmail.com>
2021-04-19 11:08:21 -0700

    Add User model, slogin/register user routes, and validatons

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.get('/test', (req, res) => res.json({msg: "The users' router is working"}))
+const User = require('../../models/User');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+
+router.get('/test', (req, res) => res.json({msg: "The users' router is working"}));
+
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Use the validations to send the error
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+      }
+    });
+});
+
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        // Use the validations to send the error
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({msg: 'Success'});
+          } else {
+            // And here:
+            errors.password = 'Incorrect password';
+            return res.status(400).json(errors);
+          }
+        });
+    });
+});
+
+
 
 module.exports = router;
 
