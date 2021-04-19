5c1231e8e5015df774fbada05b2817700d70941b
Andy Huang <andy.k.huang@gmail.com>
2021-04-19 12:31:56 -0700

    Add passport authentication. Modify register and login routes to check for duplicates and to return user data

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const mongoose = require('mongoose');
 const express = require('express');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
+const passport = require('passport');
 
 
 // setup connection with the MongoDB
 mongoose
-.connect(db, {useNewUrlParser: true})
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 //sets up Express web framework 
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.get('/', (req, res) => res.send("Hello World"));
+
+//passport setup 
+app.use(passport.initialize()); //middleware for passport
+require('./config/passport')(passport);
+
 app.use("/api/users", users); // give access to methods and APIs from user.js
 
 // tell app which port to run on, production port or localhost:5000
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 const User = require('../../models/User');
 const validateRegisterInput = require('../../validation/register');
@@ -10,58 +12,95 @@ const validateLoginInput = require('../../validation/login');
 
 router.get('/test', (req, res) => res.json({msg: "The users' router is working"}));
 
-router.post('/register', (req, res) => {
+router.get('/current', 
+  passport.authenticate('jwt', {session: false}), 
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      username: req.user.username,
+      email: req.user.email 
+    })
+  }
+)
+
+
+router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ email: req.body.email })
+  User.findOne({ username: req.body.username })
     .then(user => {
       if (user) {
-        // Use the validations to send the error
-        errors.email = 'Email already exists';
+        errors.username = "User already exists";
         return res.status(400).json(errors);
       } else {
+        //need to add validation to see if email is already used. Or do we even need it if Mongoose already has a unique option set?
         const newUser = new User({
-          name: req.body.name,
+          username: req.body.username,
           email: req.body.email,
           password: req.body.password
+        });
+
+        bcrypt.genSalt(12, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                const payload = { id: user.id, username: user.username };
+
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                });
+              })
+              .catch(err => console.log(err));
+          });
         });
       }
     });
 });
 
-router.post('/login', (req, res) => {
+
+router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  const email = req.body.email;
+  const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email})
-    .then(user => {
-      if (!user) {
-        // Use the validations to send the error
-        errors.email = 'User not found';
-        return res.status(404).json(errors);
-      }
+  User.findOne({ username }).then(user => {
+    if (!user) {
+      errors.username = "This user does not exist";
+      return res.status(400).json(errors);
+    }
 
-      bcrypt.compare(password, user.password)
-        .then(isMatch => {
-          if (isMatch) {
-            res.json({msg: 'Success'});
-          } else {
-            // And here:
-            errors.password = 'Incorrect password';
-            return res.status(400).json(errors);
-          }
-        });
-    });
+    bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          const payload = { id: user.id, username: user.username };
+
+          jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          });
+        } else {
+          errors.password = "Incorrect password";
+          return res.status(400).json(errors);
+        }
+      });
+  });
 });
 
 
