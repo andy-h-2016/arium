6cb37a19b1dddcf8c9d640d65e554eb28747adef
Andy Huang <andy.k.huang@gmail.com>
2021-04-20 14:05:11 -0700

    Move production route to after express framework is intialized

--- app.js
+++ app.js
@@ -13,18 +13,20 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
+  
+  
+  //sets up Express web framework 
+  const app = express();
+  app.use(express.urlencoded({extended: false}));
+  app.use(express.json());
 
-
-//sets up Express web framework 
-const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+  //production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 //passport setup 
 app.use(passport.initialize()); //middleware for passport
