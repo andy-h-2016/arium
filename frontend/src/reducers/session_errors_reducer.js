974fa6c38f9dd5d9482b63c98f98208d06413b1b
Mike <michaeldeanmdmph@gmail.com>
2021-04-20 12:11:12 -0700

    add next commit

--- frontend/src/reducers/session_errors_reducer.js
+++ frontend/src/reducers/session_errors_reducer.js
@@ -1,6 +1,7 @@
 import {
   RECEIVE_SESSION_ERRORS,
   RECEIVE_CURRENT_USER,
+  CLEAR_SESSION_ERRORS,
 } from '../actions/session_actions';
 
 const _nullErrors = [];
@@ -8,6 +9,8 @@ const _nullErrors = [];
 const SessionErrorsReducer = (state = _nullErrors, action) => {
   Object.freeze(state);
   switch(action.type) {
+    case CLEAR_SESSION_ERRORS:
+      return [];
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
     case RECEIVE_CURRENT_USER:
