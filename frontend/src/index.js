68f1f6022118b85f43956d3903feb40503389df7
Andy Huang <andy.k.huang@gmail.com>
2021-04-20 21:35:21 -0700

    Add store.getState method to window

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore({});
   }
   // Render our root component and pass in the store as a prop
+
+  window.getState = store.getState; 
   const root = document.getElementById('root');
 
   ReactDOM.render(<Root store={store} />, root);
