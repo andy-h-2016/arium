c7ac5200cb26eaabf2a967903d2bc6b62a6a9915
Jason Li <li.jasontse@gmail.com>
2021-04-20 21:25:35 -0700

    Finish WaterTrackers reducer

--- frontend/src/reducers/watertracker_reducer.js
+++ frontend/src/reducers/watertracker_reducer.js
@@ -3,13 +3,14 @@ import { RECEIVE_WATER_TRACKER } from '../actions/watertracker_actions';
 
 const WaterTrackersReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState;
   switch (action.type) {
     case RECEIVE_WATER_TRACKER:
-      return;
-      
-  
+      newState = Object.assign({}, state);
+      newState[action.waterTracker._id] = action.waterTracker;
+      return newState;
     default:
-      break;
+      return state;
   }
 }
 
