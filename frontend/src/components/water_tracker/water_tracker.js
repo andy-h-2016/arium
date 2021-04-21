2080b3c4c325b1f8128ac886ed33613bd8c9c71d
Jason Li <li.jasontse@gmail.com>
2021-04-21 01:51:57 -0700

    create water tracker container and component file

---
+++ frontend/src/components/water_tracker/water_tracker.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class WaterTracker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+}
+
+export default withRouter(WaterTracker);
---
+++ frontend/src/components/water_tracker/water_tracker_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import WaterTracker from './water_tracker';
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WaterTracker);
