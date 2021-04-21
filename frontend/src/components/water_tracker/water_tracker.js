d15ad182aa575bb6a29baeacb074a8a208c29c67
Jason Li <li.jasontse@gmail.com>
2021-04-21 12:38:23 -0700

    Add to Water tracker show page

--- frontend/src/components/water_tracker/water_tracker.js
+++ frontend/src/components/water_tracker/water_tracker.js
@@ -16,7 +16,10 @@ class WaterTracker extends React.Component {
     return (
       <div className="water-tracker-show-container">
         <div className="water-tracker-show-header">
-          <h1>{currentUser.username}</h1>
+          {currentUser.username}'s Water Tracker
+        </div>
+        <div className="wt-terrarium-title">
+          Put Terrariums.userId 
         </div>
       </div>
     );
