a959f08b243e3ba2181e8ce2964fb03361719127
Mike <michaeldeanmdmph@gmail.com>
2021-04-21 14:03:23 -0700

    add next commit

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,65 +1,84 @@
-import React from 'react';
+import React from "react";
 // import { receiveCurrentUser } from '../../actions/session_actions';
 
 class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = this.props.currentUser
-        this.handleSubmit = this.handleSubmit.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = this.props.currentUser;
+    this.state.update = false;
+    this.handleSubmit = this.handleSubmit.bind(this);
+   
+  }
+  componentDidMount() {
+    if (this.props.currentUser) {
+      this.props.receiveCurrentUser(this.state);
     }
-    componentDidMount(){
-      this.props.receiveCurrentUser(this.state)
-    }
+  }
 
-    handleSubmit(e) {
-      e.preventDefault();
-      const user = Object.assign({}, this.state);      
-      this.props.updateUser(user.id, user)
-      .then(this.props.receiveCurrentUser(user))
-    }
-    
-    update(field) {
-      return (e) => this.setState({ [field]: e.currentTarget.value });
-    }
-    
-    render() {
-      console.log(this.state.goal)
-          return (
-            <div className="user-edit-container">
-              <div className="user-edit-greeting">
-                Welcome {this.state.username}!             
-              </div> 
-              <form className="user-edit-form" onSubmit={this.handleSubmit}> 
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props
+      .updateUser(user.id, user)
+      .then(this.props.receiveCurrentUser(user));
+    this.state.update = true;
+  }
 
-              <div className="edit-goals">
-                <input    
+  update(field) {
+    return (e) => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  
+  render() {
+    const updated = 
+      this.state.update ? (
+        <div className="edit-message">Profile updated</div>
+      ) : (
+        <div className="edit-message">You can update your profile here.</div>
+      );
+
+    return (
+      <div className="user-edit-outter">
+        <div className="user-edit-container">
+            {updated}
+            <br/>
+          <div className="user-edit-greeting">
+            Welcome {this.state.username}! 
+          </div>
+          <br />
+          <form className="user-edit-form" onSubmit={this.handleSubmit}>
+            <div className="edit-goals">
+              This is your current water intake goal.
+              <br />
+              <input
+                className="edit-goal-input"
                 type="number"
                 onChange={this.update("goal")}
                 value={this.state.goal}
-                >                
-                </input>
-              
-              </div>
-              <div className="edit-bio">
-                <br/>      
-                <input    
+              ></input>
+              <br />
+            </div>
+            <div className="edit-bio">
+              Write a short description of your goals.
+              <br />
+              <textarea
+                className="edit-bio-textarea"
                 type="text"
                 onChange={this.update("bio")}
                 value={this.state.bio}
-                >          
-                </input>              
-              </div> 
-              <br />
-              <div className="edit-submit-button-container">
-                <button className="edit-submit-button" type='submit'>Submit Changes
-                </button>
-              </div>           
-              </form>
-
+              ></textarea>
             </div>
-          );
-        }
-     
+            <br />
+            <div className="edit-submit-button-container">
+              <button className="edit-submit-button login-signup" type="submit">
+                Submit Changes
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
