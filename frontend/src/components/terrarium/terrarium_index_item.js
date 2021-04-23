85d951c78d3b6fce7cb41f25cb96504c68bd8219
Mike <michaeldeanmdmph@gmail.com>
2021-04-22 18:08:55 -0700

    add next commit

--- frontend/src/components/terrarium/terrarium_index_item.js
+++ frontend/src/components/terrarium/terrarium_index_item.js
@@ -30,14 +30,14 @@ class TerrariumIndexItem extends React.Component {
     return (
       <div className="terrariums-index-container">
           <ul className="terra-ul-index">
-        <Link to={`/terrarium/${terrarium.userId}`}> 
+        <Link className="show-link" to={`/terrarium/${terrarium.userId}`}> 
             <div className="terra-title">
               {terrarium.title}
             </div>
-        </Link>
             <div className="terra-stages">
               <div className="t-stage">{terrariumPic}</div>
             </div>
+        </Link>
           </ul>
       </div>
     );
